1162a1fe7d40fdc32ea0ea58e553ff56e20d7549
tanvir <mdtanvirsharif@gmail.com>
2022-02-18 14:24:27 -0600

    seleniumFramework

---
+++ seleniumFramwork/src/craigList/CraigList_Example.java
@@ -0,0 +1,10 @@
+package craigList;
+
+public class CraigList_Example {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ seleniumFramwork/src/seleniumFramwork/SkySchool.java
@@ -0,0 +1,5 @@
+package seleniumFramwork;
+
+public class SkySchool {
+
+}
